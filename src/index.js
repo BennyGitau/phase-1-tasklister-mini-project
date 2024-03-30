7b740eaf79f51a30d81b9b639a296486547a2b1c
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-30 23:19:33 +0300

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createNewList(e.target)
+  })
 });
+
+
+function createNewList(addOnList){
+  let li = document.createElement('li')
+  li.innerHTML= addOnList;
+  document.querySelector('#tasks').appendChild(li)
+
+}
